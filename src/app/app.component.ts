976d5c4053f45096ccd0ef45569d4fd4c7924253
olsikule <olsi.kule@gmail.com>
2023-06-09 12:08:41 -0400

    added comments on app.components.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
+// imports the 'component' decorator from @angular/core package
 import { Component } from '@angular/core';
 
+// decorator fxn that defines and configures components
+// provides metadata and configuration for the component.
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// exports the AppComponent class so that it can be imported and used in other parts of the application
 export class AppComponent {
   title = 'angular-todo';
 }
