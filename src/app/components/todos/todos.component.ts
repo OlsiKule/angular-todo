8f816edf3a6d5f3fa19ef11b7976bc5879f5d145
olsikule <olsi.kule@gmail.com>
2023-06-09 14:00:26 -0400

    added comments on todos.components.ts

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -6,27 +6,31 @@ import { Todo } from 'src/app/models/Todo';
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
+// OnInit interface:  precursor to lifecycle hook method called ngOnInit()
 export class TodosComponent implements OnInit {
-
+// todo property with empty array
   todos: Todo[] = [];
 
+// inputTodo property with empty string
   inputTodo: string = "";
 
+  // constructor method used for dependency injection and initialization
   constructor() {}
 
+  // lifecycle hook called after component is initialized
   ngOnInit(): void {
     this.todos = [
       {
         content: "First todo",
         completed: false
       },
-      { 
+      {
         content: "Second todo",
         completed: false
       }
     ];
   }
-
+// method used to toggle the completed property
   toggleDone (id: number) {
     // value, ID as params
     this.todos.map((v, i) => {
@@ -37,12 +41,14 @@ export class TodosComponent implements OnInit {
 
   }
 
+  // method to delete a todo item from array
   deleteTodo (id: number) {
 
     this.todos = this.todos.filter((v, i) => i != id);
 
   }
 
+  // method to add a new todo item to array
   addTodo () {
     this.todos.push ({
       content: this.inputTodo,
--- src/app/models/Todo.ts
+++ src/app/models/Todo.ts
@@ -1,4 +1,5 @@
 // a blueprint for individual todo items in the app
+// represents the data structure and properties of an object, focusing on the organization and manipulation of data
 export class Todo {
   content: string = '';
   completed: boolean = false;
