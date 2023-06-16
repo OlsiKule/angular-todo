1b560632a3254025bbe515935d82509fa0e10c17
olsikule <olsi.kule@gmail.com>
2023-06-16 09:52:39 -0400

    Created a new ExpressJS application using TypeScript

--- server/server.ts
+++ server/server.ts
@@ -1,64 +1,7 @@
-import express, { Request, Response, NextFunction } from 'express';
-import { Sequelize } from 'sequelize-typescript';
-import { Task } from './models/Task';
-import { validate, handleErrors } from './middleware/validation';
-import Joi from 'joi';
-
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
-const port = 3000;
-
-// Middleware to parse JSON request bodies
-app.use(express.json());
-
-// Sequelize configuration
-const sequelize = new Sequelize({
-  // replace
-  database: 'your_database_name',
-  dialect: 'postgres',
-  // replace
-  username: 'your_username',
-  // replace
-  password: 'your_password',
-  storage: ':memory:',
-});
-
-// Add models to Sequelize
-sequelize.addModels([Task]);
-
-// Synchronize the models with the database
-sequelize.sync();
-
-// Define validation schemas
-const createTaskSchema = Joi.object({
-  title: Joi.string().required(),
-});
-
-const updateTaskSchema = Joi.object({
-  completed: Joi.boolean().required(),
-});
-
-// Define routes with validation middleware
-app.get('/tasks', (req: Request, res: Response) => {
-  // ...
-});
-
-app.post('/tasks', validate(createTaskSchema), (req: Request, res: Response) => {
-  // ...
-});
-
-app.patch('/tasks/:id', validate(updateTaskSchema), (req: Request, res: Response) => {
-  // ...
-});
-
-app.delete('/tasks/:id', (req: Request, res: Response) => {
-  // ...
-});
-
-// Error handler middleware
-app.use(handleErrors);
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.use(bodyParser.json());
+app.use(cors());
